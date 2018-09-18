ba6ee63c811379488fbad719e529f4041858d728
Shafeen M <sodl02@gmail.com>
2018-09-18 00:58:32 -0400

    added ApolloClient.service.js for angularjs

---
+++ client/ng-client/services/ApolloClient.service.js
@@ -0,0 +1,40 @@
+angular.module('basicMEAN')
+.service('ApolloClientService', ['$window', function ($window) {
+    let service = this;
+    const ApolloClient = $window.bundleRequires.ApolloBoost.default;
+
+    service.apolloClient = new ApolloClient();
+    service.gql = $window.bundleRequires.ApolloBoost.gql;
+
+    /**
+     * @param graphqlQuery
+     * @return {Promise}
+     */
+    service.query = function (graphqlQuery) {
+        return service.apolloClient.query({
+            query: graphqlQuery
+        })
+    };
+
+    service.testQuery = function () {
+        let graphqlQuery = service.gql`
+            query AllBooks {
+                books {
+                    title
+                    author {
+                        name
+                    }
+                }
+                authors {
+                    name
+                }
+            }
+        `;
+        service.query(graphqlQuery)
+        .then(response => console.log(response.data))
+        .catch(error => console.error(error));
+    };
+
+    service.testQuery();
+
+}]);
