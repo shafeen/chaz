e40d261053df147d47723b3a25b569cd20b7a625
Shafeen M <sodl02@gmail.com>
2018-09-30 21:39:59 -0400

    updated NotificationsService with a confirmDialogWarning function updated the angularjs "run" function to use the new NotificationsService.confirmDialogWarning(..) function

--- client/ng-client/run.js
+++ client/ng-client/run.js
@@ -1,19 +1,27 @@
 angular.module('basicMEAN')
-.run(['$cookies', '$http', function ($cookies, $http) {
+.run(['$cookies', '$http', 'NotificationsService', function ($cookies, $http, NotificationsService) {
     setInterval(function checkForSessionTimeout() {
         let sessionExpirationCookie = $cookies.get('sid.active');
         if (sessionExpirationCookie) {
             let timeLeftInSeconds = (parseInt(sessionExpirationCookie) - Date.now())/1000;
             if (timeLeftInSeconds < 120) {
-                // TODO: replace this code with a modal displaying a timer.
-                if (confirm('You have less than 2 minutes left before you are logged out. ' +
-                    'Do you want to continue this session?')) {
+                let text = '<strong style="color:black">Warning!</strong><br>' +
+                    'You have less than 2 minutes left before you are logged out.<br>' +
+                    'Do you want to continue this session?';
+                NotificationsService.confirmDialogWarning(text, () => {
+                    console.log('Attempting to continue session.');
                     $http.get('/api/protected/ping').catch(reason => {
                         window.location.href = '/authenticate/logout';
                     });
-                } else {
+                }, () => {
+                    console.log('Logging out from session.');
                     window.location.href = '/authenticate/logout';
-                }
+                }, () => {
+                    console.log('Dismissing warning.');
+                    $http.get('/api/protected/ping').catch(reason => {
+                        window.location.href = '/authenticate/logout';
+                    });
+                });
             }
         }
     }, 10000);
--- client/ng-client/services/Notifications.service.js
+++ client/ng-client/services/Notifications.service.js
@@ -11,16 +11,22 @@ angular.module('basicMEAN')
         ERROR: 'error'
     };
     self.layouts = {
-        TOP_RIGHT: 'topRight'
+        TOP_RIGHT: 'topRight',
+        CENTER: 'center'
+    };
+    self.defaults = {
+        timeout: 4000,
+        layout: self.layouts.TOP_RIGHT,
+        theme: 'bootstrap-v3',
     };
 
     self.notify = function (text, type, layout, isModal, timeout) {
-        timeout = (timeout===undefined)? 4000 : timeout;
+        timeout = (timeout===undefined)? self.defaults.timeout : timeout;
         const notification = new Noty({
             modal: isModal,
-            theme: 'bootstrap-v3',
+            theme: self.defaults.theme,
             type: type || self.type.INFO,
-            layout: layout || self.layouts.TOP_RIGHT,
+            layout: layout || self.defaults.layout,
             text: text,
             timeout: timeout,
             progressBar: true
@@ -47,6 +53,39 @@ angular.module('basicMEAN')
 
     self.notifyError = function (text) {
         self.notify(text, self.types.ERROR);
+    };
+
+    /**
+     * @param {string} text
+     * @param {function} onConfirm
+     * @param {function} onDecline
+     * @param {function} onClose
+     * @return {Noty}
+     */
+    self.confirmDialogWarning = function (text, onConfirm, onDecline, onClose) {
+        const confirmDialog = new Noty({
+            modal: true,
+            theme: self.defaults.theme,
+            type: self.types.WARNING,
+            layout: self.layouts.CENTER,
+            text: text,
+            buttons: [
+                Noty.button('Yes', 'btn btn-success btn-sm', function () {
+                        onConfirm();
+                        confirmDialog.close();
+                    },
+                    {id: 'confirmDialog', 'data-status': 'ok'}),
+                Noty.button('No', 'btn btn-default btn-sm', function () {
+                    onDecline();
+                    confirmDialog.close();
+                })
+            ],
+            callbacks: {
+              onClose: onClose
+            }
+        });
+        confirmDialog.show();
+        return confirmDialog;
     };
 
 }]);
