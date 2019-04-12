46f7174262826051e615d76d6ff189c432151748
Shafeen M <sodl02@gmail.com>
2019-04-12 00:01:47 -0700

    moved pug views folder to client/views

--- server/framework/app.js
+++ server/framework/app.js
@@ -35,7 +35,7 @@ metricsApp.use('/metrics', (req, res) => {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, '..', 'views'));
+app.set('views', path.join(__dirname, '..', '..', 'client', 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
